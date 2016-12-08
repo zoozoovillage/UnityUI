98542783913768107941fb59b3ca228fe5b3bc00
zoozoovillage <zoozoo.village@gmail.com>
2016-12-08 19:27:59 +0900

    add new file

---
+++ UnityUI/UI/ViewController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]	// RectTransformコンポーネントを必須にする
+public class ViewController : MonoBehaviour
+{
+	// Rect Transformコンポーネントをキャッシュ
+	private RectTransform cachedRectTransform;
+	public RectTransform CachedRectTransform
+	{
+		get {
+			if(cachedRectTransform == null)
+				{ cachedRectTransform = GetComponent<RectTransform>(); }
+			return cachedRectTransform;
+		}
+	}
+
+	// ビューのタイトルを取得、設定するプロパティ
+	public virtual string Title { get { return ""; } set {} }
+}
